fff51f7db9feb8252ab3d6ec0e6e8d7d65351d67
chimame <rito.tamata@gmail.com>
2014-07-19 13:41:18 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shinosakarb::Application.config.secret_key_base = 'ca01786c205a78cf5021ddc788cb8e35a43a69ecbafdb3f54e8ee57d229e8180b91cc4d4a6f63ca637671cb4bc6e56db6ca4c02b315a073f76ccd5092afbd57a'
